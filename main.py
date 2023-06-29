65b2c2c2c2b4f08607ef421707523daead53f357
Katadark <86430006+Katadark@users.noreply.github.com>
2023-06-29 09:51:36 -0300

    Update main.py
    Resolvendo conflito com ThLima

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from models.client import Client
 from models.motorcycle import Motorcycle
+from models.sale import Sale
 from database import create_tables
 
 # Chama a função para criar as tabelas
@@ -8,16 +9,20 @@ create_tables()
 
 def main():
     while True:
-        print("Escolha uma opção:")
-        print("1. Cadastrar um cliente")
-        print("2. Listar todos os clientes")
-        print("3. Excluir um cliente")
-        print("4. Cadastrar um motocicleta")
-        print("5. Listar todos os  motocicletas")
-        print("6. Excluir  um motocicleta")
-        print("7. Editar um cliente")
-        print("8. Editar um motocicleta")
+        print("ESCOLHA UMA OPÇÃO:")
+        print("1. Cadastrar cliente")
+        print("2. Listar clientes")
+        print("3. Excluir cliente")
+        print("4. Cadastrar motocicleta")
+        print("5. Listar motocicletas")
+        print("6. Excluir motocicleta")
+        print("7. Editar cliente")
+        print("8. Editar motocicleta")
+        print("9. Registrar venda")
+        print("10. Listar vendas")
+        print("11. Consultar venda")
         print("0. Sair")
+        print("------------------------")
 
         option = input("Opção: ")
 
@@ -30,7 +35,7 @@ def main():
         elif option == "2":
             Client.list()
         elif option == "3":
-            id_client = input("ID do client a ser excluído: ")
+            id_client = input("ID do cliente a ser excluído: ")
             Client.delete(id_client)
         elif option == "4":
             brand = input("Marca: ")
@@ -72,14 +77,32 @@ def main():
             brand = input("Nova marca: ")
             model = input("Novo modelo: ")
             year = input("Novo ano: ")
-            price = input("Novo preço: R$ ")
+            price = float(input("Novo preço: R$ "))
             motorcycle = Motorcycle(id_motorcycle, brand, model, year, price)
             motorcycle.update()
-        elif option == "0":
-            print("Saindo do programa...")
-            break
-        else:
-            print("Opção inválida. Tente novamente.")
+        elif option == "9":
+            id_client = input("ID do cliente: ")
+            id_motorcycle = input("ID da motocicleta: ")
+            sale_date = input("Data da venda (formato: dd/mm/aaaa): ")
+
+
+            # Verifica se o cliente e a motocicleta existem
+            if not Client.exists(id_client):
+                print(">>Cliente com o ID fornecido não existe!<<")
+                continue
+
+            if not Motorcycle.exists(id_motorcycle):
+                print(">>Motocicleta com o ID fornecido não existe!<<")
+                continue
+
+            # Cria a instância da venda
+            sale = Sale(None, id_client, id_motorcycle, sale_date)
+            sale.register()
+        elif option == "10":
+            Sale.list()
+        elif option == "11":
+            id_sale = input("ID da venda a ser consultada: ")
+            Sale.get_sale(id_sale)
 
 
 if __name__ == "__main__":
